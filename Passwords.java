6ec5fc0fa6f34d7b322f74bc3b1468e615dd33e2
EVOTZ62 <70346878+EVOTZ62@users.noreply.github.com>
2021-05-02 12:41:08 -0400

    Started Password class

---
+++ Passwords.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.io.PrintWriter;
+import java.util.Scanner;
+import java.io.FileReader;
+/**
+ * Write a description of class Passwords here.
+ *
+ * @author Mark Verra
+ * @version Spring 2021
+ */
+public class Passwords
+{
+    private ArrayList<String> greekLetters;
+    private int nums;
+    private String firstLetter;
+    private String secondLetter;
+    
+    public Passwords()
+    {
+        
+    }
+}
